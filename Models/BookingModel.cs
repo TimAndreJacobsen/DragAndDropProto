db766e736fbf8cd2453f1903c8c1fe78ff9d195b
Tim Andre Jacobsen <TimAndre.Jacobsen@vitecsoftware.com>
2021-08-18 15:17:30 +0200

    scaffold booking dragndrop proto

---
+++ Models/BookingModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragAndDropProto.Models
+{
+    public class BookingModel
+    {
+        public int Id { get; set; }
+        public BookingStatuses Status { get; set; }
+        public string Description { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public enum BookingStatuses
+    {
+        Free,
+        Started,
+        Completed
+    }
+}
